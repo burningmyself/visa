3921c9384e2046bd4efbf64aa6f2b2816eabaf04
yangxiaoxiao23 <yangxiaoxiao23@gmail.com>
2014-08-13 09:34:56 +0800

    初始化

---
+++ visa/Lib/Action/ServiceAction.class.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: YHB
+ * Date: 14-8-8
+ * Time: 上午10:26
+ */
+
+class ServiceAction extends  Action {
+    public function ask(){
+        $this->display();
+    }
+} 
